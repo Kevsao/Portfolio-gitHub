b01effe290365a32c5e39dc377c35a0d693d52e7
Kevsao <kevinplecispro@gmail.com>
2024-06-29 16:54:36 +0200

    ajout index html

---
+++ Portfolio/HTML/traitement.php
@@ -0,0 +1,9 @@
+<?php
+if(isset($_post['ok'])){
+    $name = $_post['name'];
+    $prename = $_post['prename'];
+    $email = $_post['email'];
+    $subject = $_post['subject'];
+    $message = $_post['message'];
+}
+?>
