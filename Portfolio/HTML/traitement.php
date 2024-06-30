7e16d1d46a9dd6120b18b365defc173e37a95210
Kevsao <kevinplecispro@gmail.com>
2024-06-30 21:31:15 +0200

    Delete Portfolio directory

--- Portfolio/HTML/traitement.php
+++
@@ -1,9 +0,0 @@
-<?php
-if(isset($_post['ok'])){
-    $name = $_post['name'];
-    $prename = $_post['prename'];
-    $email = $_post['email'];
-    $subject = $_post['subject'];
-    $message = $_post['message'];
-}
-?>
